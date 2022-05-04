20d715cef09395fb6abaac555d6f3a310ac7df2a
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-04 17:00:49 +0300

    Commit

--- book_data.js
+++ book_data.js
@@ -1,11 +1,11 @@
 // url = https://bookmate.com/@babenkodmitry/books/all
 
-var container = document.getElementsByClassName("book book_grid  book_88")[0];
-var status = container.querySelector("span").innerText;
-var cover = container.querySelector("img").getAttribute("src");
-var title = container.querySelector("a").getAttribute("title");
-var author = container.querySelector("img").getAttribute("alt").split(", ")[1];
-var url = "https://bookmate.com" + container.querySelector("a").getAttribute("href");
+const container = document.getElementsByClassName("book book_grid  book_88")[0];
+const status = container.querySelector("span").innerText;
+const cover = container.querySelector("img").getAttribute("src");
+const title = container.querySelector("a").getAttribute("title");
+const author = container.querySelector("img").getAttribute("alt").split(", ")[1];
+const url = "https://bookmate.com" + container.querySelector("a").getAttribute("href");
 
 headers = ["Progress","Cover","Title","Author","Read"]
 bookAttributes = [status,cover,title,author,url];
--- book_excerption.js
+++ book_excerption.js
@@ -1,4 +1,4 @@
 // url = https://bookmate.com/reader/saTDRM1P?resource=book
 
-var excerption = document.getElementsByClassName("tni")[2].innerText;
+const excerption = document.getElementsByClassName("tni")[2].innerText;
 console.log(excerption)
