ed2ad2f3091d2e2801e6aa6fb3f5783d43084842
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-08 16:55:39 +0300

    Commit

--- book_data.js
+++ book_data.js
@@ -1,24 +1,20 @@
 // "https://bookmate.com/@babenkodmitry/books/all"
 
-// const input = prompt("URL: ");
+const input = prompt("URL: ");
 
-// const container = document.getElementsByClassName("book book_grid  book_88")[0];
-// const status = container.querySelector("span").innerText;
-// const cover = container.querySelector("img").getAttribute("src");
-// const title = container.querySelector("a").getAttribute("title");
-// const author = container.querySelector("img").getAttribute("alt").split(", ")[1];
-// const url = "https://bookmate.com" + container.querySelector("a").getAttribute("href");
-//
-// headers = ["Title","Author","Progress","Cover","Url"]
-// bookAttributes = [title,author,status,cover,url];
-// bookData = {};
-// for (var i = 0; i < headers.length; i++) {
-//   bookData[headers[i]] = bookAttributes[i]
-// };
-//
-// console.log(bookData)
+const container = document.getElementsByClassName("book book_grid  book_88")[0];
+const status = container.querySelector("span").innerText;
+const cover = container.querySelector("img").getAttribute("src");
+const title = container.querySelector("a").getAttribute("title");
+const author = container.querySelector("img").getAttribute("alt").split(", ")[1];
+const url = "https://bookmate.com" + container.querySelector("a").getAttribute("href");
 
-bookData = {Title: 'To Sleep in a Sea of Stars', Author: 'Christopher Paolini', Progress: '8%', Cover: 'https://assets1.bmstatic.com/assets/books-covers/97/5c/ipad-e5265287f0737e5e462bb046bc663305.jpeg', Url: 'https://bookmate.com/reader/saTDRM1P?resource=book'}
+headers = ["Title","Author","Progress","Cover","Url"]
+bookAttributes = [title,author,status,cover,url];
+bookData = {};
+for (var i = 0; i < headers.length; i++) {
+  bookData[headers[i]] = bookAttributes[i]
+};
 
 var bookTitle = document.querySelector('.header_container');
 var bookAuthor = document.querySelector('.author_container');
